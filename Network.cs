670304f1c22b22b16c7abe1849adcd9bb254aa1d
ruthl7 <nemes.balazs.ferenc@students.jedlik.eu>
2024-01-01 19:11:26 +0100

    finished

--- Network.cs
+++ Network.cs
@@ -1,54 +1,55 @@
-﻿using System;
-using System.IO;
-using System.Net;
-using System.Net.NetworkInformation;
-using System.Net.Sockets;
-using System.Text;
+﻿
+//using System;
+//using System.IO;
+//using System.Net;
+//using System.Net.NetworkInformation;
+//using System.Net.Sockets;
+//using System.Text;
 
-namespace Torpedo
-{
-    public static class Network
-    {
-        internal class HalozatiAdatok
-        {
-            public static string sajatDomain = Dns.GetHostName();
-            public static IPAddress sajatIp = Dns.GetHostAddresses(sajatDomain)[2];
-            public static IPAddress ellenfelIP;
-            public static IPEndPoint celallomas;
-            public static TcpClient kuldes = new TcpClient();
-            public static TcpListener listener = new(sajatIp, 1000);
-            public static TcpClient fogadas = new TcpClient();
-            public static NetworkStream stream = fogadas.GetStream();
-            public static StreamReader olvas = new StreamReader(stream);
-            public static StreamWriter ir = new StreamWriter(stream);
-        }
+//namespace Torpedo
+//{
+//    public static class Network
+//    {
+//        internal class HalozatiAdatok
+//        {
+//            public static string sajatDomain = Dns.GetHostName();
+//            public static IPAddress sajatIp = Dns.GetHostAddresses(sajatDomain)[2];
+//            public static IPAddress ellenfelIP = IPAddress.Parse("192.168.1.2");
+//            public static IPEndPoint celallomas;
+//            public static TcpClient kuldes = new TcpClient();
+//            public static TcpListener listener = new(sajatIp, 1000);
+//            public static TcpClient fogadas = new TcpClient();
+//            public static NetworkStream stream = fogadas.GetStream();
+//            public static StreamReader olvas = new StreamReader(stream);
+//            public static StreamWriter ir = new StreamWriter(stream);
+//        }
 
-        public static void Csatlakozas()
-        {
-             // Retrive the Name of HOST
-            Console.WriteLine(HalozatiAdatok.sajatDomain);
-            // Get the IP
-            
-            Console.WriteLine($"A te IP címed: {HalozatiAdatok.sajatIp}");
-            Console.WriteLine("Írd be a másik játékos IP címét: ");
-            HalozatiAdatok.ellenfelIP = IPAddress.Parse(Console.ReadLine());
-            HalozatiAdatok.celallomas = new IPEndPoint(HalozatiAdatok.ellenfelIP, 1001);
-            HalozatiAdatok.kuldes.Connect(HalozatiAdatok.celallomas);
-            Console.WriteLine("Meghívó elküldve...");
-            if(HalozatiAdatok.kuldes.Connected)
-                Console.WriteLine("Kapcsolat kész!");
-        }
+//        public static void Csatlakozas()
+//        {
+//            // Retrive the Name of HOST
+//            Console.WriteLine(HalozatiAdatok.sajatDomain);
+//            // Get the IP
 
-        public static void Kuldes(string tipp)
-        {
-           HalozatiAdatok.ir.AutoFlush = true;
-           HalozatiAdatok.ir.Write(tipp);
-        }
+//            Console.WriteLine($"A te IP címed: {HalozatiAdatok.sajatIp}");
+//            Console.WriteLine("Írd be a másik játékos IP címét: ");
+//            HalozatiAdatok.ellenfelIP = IPAddress.Parse(Console.ReadLine());
+//            HalozatiAdatok.celallomas = new IPEndPoint(HalozatiAdatok.ellenfelIP, 1001);
+//            HalozatiAdatok.kuldes.Connect(HalozatiAdatok.celallomas);
+//            Console.WriteLine("Meghívó elküldve...");
+//            if (HalozatiAdatok.kuldes.Connected)
+//                Console.WriteLine("Kapcsolat kész!");
+//        }
 
-        public static string Fogadas()
-        {
-            string szoveg = HalozatiAdatok.olvas.ReadLine();
-            return szoveg;
-        }
-    }
-}
+//        public static void Kuldes(string tipp)
+//        {
+//            HalozatiAdatok.ir.AutoFlush = true;
+//            HalozatiAdatok.ir.Write(tipp);
+//        }
+
+//        public static string Fogadas()
+//        {
+//            string szoveg = HalozatiAdatok.olvas.ReadLine();
+//            return szoveg;
+//        }
+//    }
+//}
--- Program.cs
+++ Program.cs
@@ -1,165 +1,212 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading;
 using Torpedo;
 
-
-Console.OutputEncoding = System.Text.Encoding.UTF8;
-Mezo[] mezok = new Mezo[100];
-Mezo[] ellenfel = new Mezo[100];
-Console.WriteLine("Szia! Hogy hívnak?");
-string name = Console.ReadLine();
-Console.WriteLine("Szia, " + name + "!");
-
-bool HajoLerak(Mezo mezo)
+public class Game
 {
-    if (mezo.hajo)
-    {
-        Console.WriteLine("Erre a mezőre már raktál hajót!");
-        Thread.Sleep(1000);
-        return false;
-    }
-    else
-    {
-        mezo.hajo = true;
-        return true;
-    }
-}
+    private Random random = new Random();
+    private Mezo[] jatekos = new Mezo[100];
+    private Mezo[] ellenfel = new Mezo[100];
+    private List<int[]> botTalalatok = new List<int[]>();
 
-bool Tippeles(Mezo mezo)
-{
-    if (mezo.kilove)
+    public void StartGame()
     {
-        Console.WriteLine("Erre a mezőre már tippeltél!");
-        Thread.Sleep(1000);
-        return false;
-    }
-    else
-    {
-        mezo.kilove = true;
-        return true;
-    }
-}
+        Console.OutputEncoding = Encoding.UTF8;
 
-static void Generalas(Mezo[] mezok, Mezo[] ellenfel)
-{
-    int sorszam = 0;
-    int sor = 1;
-    for (int i = 1; i < 11; i++)
+        Console.WriteLine("Szia! Hogy hívnak?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Szia, " + name + "!");
+
+        Generalas();
+        Lerakas();
+        Jatek();
+    }
+
+    private bool HajoLerak(bool bot, Mezo mezo)
     {
-        sor = 1;
-        while (sor != 11)
+        if (mezo.hajo)
         {
-            mezok[sorszam] = new Mezo(i, sor, false, false);
-            ellenfel[sorszam] = new Mezo(i, sor, false, false);
-            sor++;
-            sorszam++;
+            if (!bot)
+            {
+                Console.WriteLine("Erre a mezőre már raktál hajót!");
+                Thread.Sleep(1500);
+            }
+            return false;
+        }
+        else
+        {
+            mezo.hajo = true;
+            return true;
         }
     }
-}
 
-void Lerakas()
-{
-    Dictionary<string, int> hajok = new Dictionary<string, int> {
-    { "Repülőgéphordozó", 5 }, { "Csatahajó", 4}, { "Cirkáló", 3 } , { "Tengeralattjáró", 3 } ,{ "Torpedóromboló", 2 }};
-    foreach (var hajo in hajok.Keys)
+    private void Generalas()
     {
-        // Lerakás ellenőrzés
-        int hajohossz = hajok[hajo];
+        int sorszam = 0;
+        int sor = 1;
+        for (int i = 1; i < 11; i++)
+        {
+            sor = 1;
+            while (sor != 11)
+            {
+                jatekos[sorszam] = new Mezo(i, sor, false, false);
+                ellenfel[sorszam] = new Mezo(i, sor, false, false);
+                sor++;
+                sorszam++;
+            }
+        }
+    }
+
+    private Mezo Mezokeres(Mezo[] tabla, int oszlop, int sor)
+    {
+        Mezo mezo = tabla.First(x => x.oszlop == oszlop && x.sor == sor);
+        return mezo;
+    }
+
+    private void ellenfelLetrehoz(int hajohossz, List<int[]> hajokLerakva)
+    {
+        int[] tipp = new int[2];
+        int oszlop = 0;
+        int sor = 0;
         List<int> oszlopok = new List<int> { };
         List<int> sorok = new List<int> { };
-        Console.WriteLine($"\nRakd le a {hajo}t! ({hajohossz})");
         do
         {
-            ; var hely = Beker();
-            int oszlop = hely[0];
-            int sor = hely[1];
-            Mezo mezo = mezok.First(x => x.oszlop == oszlop && x.sor == sor);
-            if (HajoLerak(mezo) && Ellenorzes(oszlop, sor, oszlopok, sorok))
+            oszlop = tipp[0] = random.Next(1, 11);
+            sor = tipp[1] = random.Next(1, 11);
+            Mezo mezo = Mezokeres(ellenfel, oszlop, sor);
+            if (HajoLerak(true, mezo) && Ellenorzes(true, oszlop, sor, oszlopok, sorok))
             {
                 oszlopok.Add(mezo.oszlop);
                 sorok.Add(mezo.sor);
                 hajohossz--;
             }
-
-            Kiiaratas(true);
         }
         while (hajohossz > 0);
-        Console.WriteLine("\nKész!");
-        Thread.Sleep(1000);
     }
 
-    bool Ellenorzes(int oszlop, int sor, List<int> oszlopok, List<int> sorok)
+    private void Lerakas()
+    {
+        Dictionary<string, int> hajok = new Dictionary<string, int> {
+            { "Repülőgéphordozó", 5 }, { "Csatahajó", 4}, { "Cirkáló", 3 } , { "Tengeralattjáró", 3 } ,{ "Torpedóromboló", 2 }};
+        List<int[]> hajokLerakva = new List<int[]>();
+        foreach (var hajo in hajok.Keys)
+        {
+            int hajohossz = hajok[hajo];
+            ellenfelLetrehoz(hajohossz, hajokLerakva);
+            List<int> oszlopok = new List<int> { };
+            List<int> sorok = new List<int> { };
+            Console.WriteLine($"\nRakd le a {hajo}t! ({hajohossz})");
+            do
+            {
+                var hely = Beker();
+                int oszlop = hely[0];
+                int sor = hely[1];
+                Mezo mezo = Mezokeres(jatekos, oszlop, sor);
+                if (HajoLerak(false, mezo) && Ellenorzes(false, oszlop, sor, oszlopok, sorok))
+                {
+                    oszlopok.Add(mezo.oszlop);
+                    sorok.Add(mezo.sor);
+                    hajohossz--;
+                }
+                Kiiaratas(true);
+            }
+            while (hajohossz > 0);
+            Console.WriteLine("\nKész!");
+            Thread.Sleep(1000);
+        }
+    }
+
+    private bool Ellenorzes(bool bot, int oszlop, int sor, List<int> oszlopok, List<int> sorok)
     {
         bool helyes = false;
-        if (oszlopok.Count() == 0) // ha még nem adott meg semmit
+        if (oszlopok.Count() == 0)
             return true;
-        if (oszlopok.Max() - oszlopok.Min() == 0 && oszlopok[0] == oszlop) //Azonos-e valamelyik sorral
+        if (oszlopok.Max() - oszlopok.Min() == 0 && oszlopok[0] == oszlop)
             helyes = true;
-        if (sorok.Max() - sorok.Min() == 0 && sorok[0] == sor)             //,vagy oszloppal
+        if (sorok.Max() - sorok.Min() == 0 && sorok[0] == sor)
             helyes = true;
-        if ((helyes == true) && (sorok.Max() == sor - 1 || sorok.Min() == sor + 1 || oszlopok.Max() == oszlop - 1 || oszlopok.Min() == oszlop + 1))  //Ha már létező mellé rakta
+        if ((helyes == true) && (sorok.Max() == sor - 1 || sorok.Min() == sor + 1 || oszlopok.Max() == oszlop - 1 || oszlopok.Min() == oszlop + 1))
             return helyes;
         else
         {
-            Console.WriteLine("A darabokat sorban rakd le!");
+            if (!bot)
+            {
+                Console.WriteLine("A darabokat sorban rakd le!");
+                Thread.Sleep(1500);
+                Mezokeres(jatekos, oszlop, sor).hajo = false;
+            }
+            else
+                Mezokeres(ellenfel, oszlop, sor).hajo = false;
         }
-        if (helyes == false)
+        if (helyes == false && !bot)
+        {
             Console.WriteLine("A hajódnak egyesnek kell lennie!");
-        Thread.Sleep(1500);
-        mezok.First(x => x.oszlop == oszlop && x.sor == sor).hajo = false;
+            Thread.Sleep(1500);
+        }
+
         return false;
     }
-}
 
-int[] Beker()
-{
-    int hibas = 0;
-    int[] koordinatak = new int[2];
-    string[] bekert = new string[] { };
-    while (true)
+    private int[] Beker()
     {
-        if (hibas > 0)
-            Console.WriteLine("Két külön számot adj meg 1 és 10 között. \nPélda: 5 5");
-        Console.WriteLine("\n\nAdd meg a koordinátákat (oszlop,sor): ");
-        bekert = Console.ReadLine().Trim().Split(" ");
-        hibas++;
-        if (bekert.Length != 2 || (!int.TryParse(bekert[0], out int o) || !int.TryParse(bekert[1], out int s)))
-            continue;
-        koordinatak[0] = int.Parse(bekert[0]);
-        koordinatak[1] = int.Parse(bekert[1]);
-        if (koordinatak[0] <= 10 && koordinatak[0] >= 1 && koordinatak[1] <= 10 && koordinatak[1] >= 1)
-            break;
-    }
-    return koordinatak;
-}
-
-
-void Kiiaratas(bool lerakas)
-{
-
-    Console.Clear();
-    Console.Write("\t");
-    for (int oszlop = 1; oszlop < 11; oszlop++) //Számok
-    {
-        Console.Write($" {oszlop} ");
-    }
-    Console.Write("\n1\t");
-    for (int i = 0; i < mezok.Length; i++)
-    {
-        try //Oszlop számok
+        int hibas = 0;
+        int[] koordinatak = new int[2];
+        string[] bekert = new string[] { };
+        while (true)
         {
-            if (mezok[i - 1].sor % 10 == 0)
-                Console.Write($"\n{(i / 10) + 1}\t");
+            if (hibas > 0)
+                Console.WriteLine("Két külön számot adj meg 1 és 10 között. \nPélda: 5 5");
+            Console.WriteLine("\n\nAdd meg a koordinátákat (oszlop,sor): ");
+            bekert = Console.ReadLine().Trim().Split(" ");
+            hibas++;
+            if (bekert.Length != 2 || (!int.TryParse(bekert[0], out int o) || !int.TryParse(bekert[1], out int s)))
+                continue;
+            koordinatak[0] = int.Parse(bekert[0]);
+            koordinatak[1] = int.Parse(bekert[1]);
+            if (koordinatak[0] <= 10 && koordinatak[0] >= 1 && koordinatak[1] <= 10 && koordinatak[1] >= 1)
+                break;
         }
-        catch
-        {
-            if (mezok[i].sor > 10)
-                Console.WriteLine("\t");
-        }
+        return koordinatak;
+    }
 
+    private void Kiiaratas(bool lerakas)
+    {
+        Mezo[] tomb;
         if (lerakas)
+            tomb = jatekos;
+        else
+            tomb = ellenfel;
+
+        Console.Clear();
+        Console.Write("\t");
+        for (int oszlop = 1; oszlop < 11; oszlop++)
         {
-            if (mezok[i].hajo)
+            Console.Write($" {oszlop} ");
+        }
+        Console.Write("\n1\t");
+        for (int i = 0; i < tomb.Length; i++)
+        {
+            try
+            {
+                if (jatekos[i - 1].sor % 10 == 0)
+                    Console.Write($"\n{(i / 10) + 1}\t");
+            }
+            catch
+            {
+                if (jatekos[i].sor > 10)
+                    Console.WriteLine("\t");
+            }
+
+            if (!lerakas && !tomb[i].kilove)
+            {
+                Console.Write("[?]");
+                continue;
+            }
+            if (tomb[i].hajo)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write(" O ");
@@ -172,172 +219,119 @@ void Kiiaratas(bool lerakas)
                 Console.ForegroundColor = ConsoleColor.White;
             }
         }
+    }
+
+    private int jatekosTalalat = 17;
+    private int botTalalat = 17;
+
+    private void Jatek()
+    {
+
+        Console.WriteLine("Most támadhatsz!");
+        Thread.Sleep(1500);
+        while (jatekosTalalat > 0 && botTalalat > 0)
+        {
+            Tamadas();
+            Console.WriteLine("Most az ellenfél jön!");
+            Thread.Sleep(1500);
+            Bot();
+            Console.WriteLine($"{botTalalat} hajód maradt\n Az ellenfélnek {jatekosTalalat} hajója maradt");
+            Thread.Sleep(1500);
+        }
+        if (jatekosTalalat == 0)
+        {
+            Console.WriteLine("Gratulálunk, nyertél!");
+            Thread.Sleep(2500);
+        }
         else
         {
-            if (ellenfel[i].kilove)
-                Console.Write("[?]");
+            Console.WriteLine("Az ellenfél nyert!");
+            Thread.Sleep(2500);
+        }
 
-            else
+    }
+
+    private void Tamadas()
+    {
+        while (true)
+        {
+            Kiiaratas(false);
+            var tipp = Beker();
+            Mezo meglottMezo = Mezokeres(ellenfel, tipp[0], tipp[1]);
+            if (!meglottMezo.kilove)
             {
-                if (ellenfel[i].hajo)
+                if (meglottMezo.hajo)
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write(" O ");
-                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Talált!");
+                    jatekosTalalat--;
+                    Thread.Sleep(800);
+                    meglottMezo.kilove = true;
+                    continue; //ha talált jöhet még egyszer
                 }
                 else
                 {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.Write(" X ");
-                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Nem talált!");
+                    Thread.Sleep(800);
+                    break;
                 }
             }
+            else
+            {
+                Console.WriteLine("Erre a mezőre már tippeltél!");
+                Thread.Sleep(800);
+            }
+            meglottMezo.kilove = true;
+        }
+    }
+
+    private void Bot()
+    {
+        int[] tipp = new int[2];
+        while (true)
+        {
+            if (botTalalatok.Count == 0)
+            {
+                tipp[0] = random.Next(1, 11);
+                tipp[1] = random.Next(1, 11);
+            }
+            else
+            {
+                int sorVagyOszlop = random.Next(0, 2);
+                var tampont = botTalalatok[random.Next(0, botTalalatok.Count + 1)];
+                tipp[sorVagyOszlop] = tampont[sorVagyOszlop] + random.Next(-1, 2);
+                tipp[1 - sorVagyOszlop] = tampont[1 - sorVagyOszlop];
+            }
+
+            Mezo meglottMezo = Mezokeres(jatekos, tipp[0], tipp[1]);
+            if (meglottMezo.kilove)
+                continue;
+            else
+            {
+                if (meglottMezo.hajo)
+                {
+                    Console.WriteLine("Robot talált!");
+                    botTalalat--;
+                    Thread.Sleep(1200);
+                }
+                else
+                {
+                    Console.WriteLine("Robot nem talált!");
+                    Thread.Sleep(1200);
+                    meglottMezo.kilove = true;
+                    break;
+                }
+            }
+            meglottMezo.kilove = true;
+            Console.WriteLine(meglottMezo.oszlop + "  " + meglottMezo.sor);
         }
     }
 }
 
-void Jatek()
+class Program
 {
-    while (true)
+    static void Main()
     {
-        Tamadas();
+        Game game = new Game();
+        game.StartGame();
     }
-
-    void Tamadas()
-    {
-        string tipp = Beker().ToString();
-        Network.Kuldes(tipp);
-        Console.WriteLine(Network.Fogadas());
-    }
-
-    void Vedekezes()
-    {
-
-
-    }
-
 }
-
-Generalas(mezok, ellenfel);
-Lerakas();
-Jatek();
-
-//void Tamadas(int jatekos) //1 vagy 2
-//{
-//    int lepes = 0;
-//    int talalt = 0;
-//    int[] koordinatak = Beker();
-//    string[,] TamadasJatekTer = JatekTer;
-//    if (jatekos == 1)
-//    {
-//        StreamWriter sw = new StreamWriter("player1tamadas.txt");
-//        /*JatekTer[x - 1, y - 1] = "X";*/
-//        if (Kiiaratas(false)[x - 1, y - 1] == "X")
-//        {
-//            sw.WriteLine($"{lepes + 1}. lépés: talált({koordinatak[0]} {koordinatak[1]})");
-//            talalt++;
-//            lepes++;
-//        }
-//        else
-//        {
-//            sw.WriteLine($"{lepes + 1}. lépés: nem talált({koordinatak[0]} {koordinatak[1]})");
-//            lepes++;
-//        }
-//        sw.Close();
-//    }
-//    else
-//    {
-//        StreamWriter sw = new StreamWriter("player2tamadas.txt");
-//        /*JatekTer[x - 1, y - 1] = "X";*/
-//        if (TamadasJatekTer[x - 1, y - 1] == "X")
-//        {
-//            sw.WriteLine($"{lepes + 1}. lépés: talált({koordinatak[0]} {koordinatak[1]})");
-//            talalt++;
-//            lepes++;
-//        }
-//        else
-//        {
-//            sw.WriteLine($"{lepes + 1}. lépés: nem talált({koordinatak[0]} {koordinatak[1]})");
-//            lepes++;
-//        }
-//        sw.Close();
-//    }
-
-//}
-
-//void Bot()
-//{
-//    Random random = new Random();
-//    int y = random.Next(0, 9);
-//    int x = random.Next(0, 9);
-//    StreamWriter sw = new StreamWriter("bottamadas.txt");
-//    /*JatekTer[x - 1, y - 1] = "X";*/
-//    sw.WriteLine($"{y} {x}");
-//    sw.Close();
-//}
-
-//Bot();
-
-
-//int hajoDb = 5;
-//string[,] JatekTer = new string[10, 10];
-//string[] BekertKordinata = new string[2];
-//AlapJatekTer();
-//bool isGameOver = false;
-
-
-//do
-//{
-//    Console.WriteLine($"\t{hajoDb} Hajórészed van még hátra!");
-
-//    Console.Write("\tAdja meg a hajó darabjának kordinátáját (oszlop sor) : ");
-//    BekertKordinata = Console.ReadLine().Split(" ");
-
-//    int y = int.Parse(BekertKordinata[0]);
-//    int x = int.Parse(BekertKordinata[1]);
-
-
-//    JatekTerKiir(x, y);
-
-//    hajoDb --;
-//    Console.WriteLine();
-//} while (hajoDb != 0);
-
-
-
-//string JatekTerKiir(int x, int y)
-//{
-//    JatekTer[x-1, y-1] = "X";
-//    Console.WriteLine();
-//    for (int i = 0; i < JatekTer.GetLength(0); i++)
-//    {
-//        Console.Write("\t");
-//        for (int j = 0;j < JatekTer.GetLength(1); j++)
-//        {
-//            Console.Write(JatekTer[i,j]);
-//        }
-//        Console.WriteLine("\t");
-//    }
-//    return "";
-//}
-
-
-//string AlapJatekTer()
-//{
-//    for (int i = 0; i < JatekTer.GetLength(0); i++)
-//    {
-//        for (int j = 0; j < JatekTer.GetLength(1); j++)
-//        {
-//            JatekTer[i, j] = "0";
-//        }
-
-//    }
-//    return "";
-//}
-
-// void Tamadas()
-// {
-//     string tipp = Beker().ToString();
-//     Network.Kuldes(tipp);
-//     Console.WriteLine(Network.Fogadas());
-// }
